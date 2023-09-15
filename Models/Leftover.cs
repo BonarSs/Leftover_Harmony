89bca8803916f4643cdb0a3cd0ddd5760b3759be
hisazakura <luthfanarrayyan@gmail.com>
2023-09-15 14:44:16 +0700

    Create base project structure

---
+++ Models/Leftover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leftover_Harmony.Models
+{
+    internal class Leftover
+    {
+        private string _name;
+        private string _description;
+        private int _quantity;
+        private Category _category;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Description { get { return _description; } set { _description = value; } }
+        public int quantity { get { return _quantity; } set { _quantity = value; } }
+        public Category Category { get { return _category; } set { _category = value; } }
+        public Leftover(string name, string description, int quantity, Category category)
+        {
+            _name = name;
+            _description = description;
+            _quantity = quantity;
+            _category = category;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,3 @@ Ketua Kelompok: \
 Anggota 1: Muhammad Luthfan Arrayyan - 21/479825/TK/52943\
 Anggota 2: Partahi Bonaruli Sitorus - 21/482615/TK/53324\
 Anggota 3: Pranawa Dinanta Adhyarta Putra - 21/482959/TK/53395
-
