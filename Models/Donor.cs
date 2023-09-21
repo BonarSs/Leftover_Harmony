757507e73028cd21c8c04a1c13347a8da9668b30
hisazakura <luthfanarrayyan@gmail.com>
2023-09-21 13:40:24 +0700

    Updated Donor and Request class

--- Models/Donor.cs
+++ Models/Donor.cs
@@ -8,12 +8,21 @@ namespace Leftover_Harmony.Models
 {
     internal class Donor : User
     {
+        private List<Request> _requests;
+
         public Donor(string username, string password, string email, string phoneNumber) : base(username, password, email, phoneNumber)
         {
             Username = username;
             Password = password;
             Email = email;
             PhoneNumber = phoneNumber;
+        }
+
+        public bool Donate(Request request)
+        {
+            if (request == null) return false;
+            request.AddDonor(this);
+            return true;
         }
     }
 }
